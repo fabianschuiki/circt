ac67669906de5eaabae2d20bd501a19db147e107
Fabian Schuiki <fabian@schuiki.ch>
2022-06-10 16:16:01 +0200

    [LowerToHW] Add BackedgeBuilder, directly materialize inst inputs
    Add a `BackedgeBuilder` to `FIRRTLLowering` which can be used to
    directly materialize values that violate def-before-use ordering. To
    simplify usage, a new `setBackedgeLowering` function creates a backedge
    with the desired type and memorizes it as the lowering for the original
    value. A later call to `setLowering` for the original value will resolve
    the backedge.
    
    As a first use of the backedge builder, materialize the input values for
    instances directly through a backedge if the port is trivially assigned
    by a single `StrictConnectOp`. This does away with temporary wires in
    most cases and is a useful preparation for handling non-FIRRTL types
    during lowering.

--- include/circt/Dialect/FIRRTL/FIRRTLOps.h
+++ include/circt/Dialect/FIRRTL/FIRRTLOps.h
@@ -30,6 +30,8 @@
 namespace circt {
 namespace firrtl {
 
+class StrictConnectOp;
+
 // is the name useless?
 bool isUselessName(circt::StringRef name);
 
@@ -101,6 +103,12 @@ bool hasDontTouch(Value value);
 /// Check whether an operation has a `DontTouch` annotation, or a symbol that
 /// should prevent certain types of canonicalizations.
 bool hasDontTouch(Operation *op);
+
+/// Scan all the uses of the specified value, checking to see if there is
+/// exactly one connect that sets the value as its destination.  This returns
+/// the operation if found and if all the other users are "reads" from the
+/// value.
+StrictConnectOp getSingleConnectUserOf(Value value);
 
 // Out-of-line implementation of various trait verification methods and
 // functions commonly used among operations.
