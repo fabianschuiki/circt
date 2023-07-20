2878df376d9a2b3f0a4f83f386649d33eccb8c2c
hailong <hailong.sun@terapines.com>
2023-07-20 13:52:17 +0800

    Add support for the moore::UnpackedQueueDim.

--- lib/Conversion/ImportVerilog/Types.cpp
+++ lib/Conversion/ImportVerilog/Types.cpp
@@ -61,6 +61,19 @@ struct TypeVisitor {
         packedInnerType, moore::Range(type.range.left, type.range.right));
   }
 
+  Type visit(const slang::ast::QueueType &type) {
+    auto innerType = type.elementType.visit(*this);
+    if (!innerType)
+      return {};
+    auto unpackedInnerType = dyn_cast<moore::UnpackedType>(innerType);
+    if (!unpackedInnerType) {
+      mlir::emitError(loc, "dynamic unpacked array; ")
+          << type.elementType.toString() << "is queue unpacked";
+      return {};
+    }
+    return moore::UnpackedQueueDim::get(unpackedInnerType, type.maxBound);
+  }
+
   /// Emit an error for all other types.
   template <typename T>
   Type visit(T &&node) {
